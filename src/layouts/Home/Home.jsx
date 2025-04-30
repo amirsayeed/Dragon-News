32e6fdbda6ea102ac784c19ebaa003acd67f5bbb
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-05-01 01:47:42 +0600

    initial project setup with tailwind and daisyUI, initial route setup with react router

---
+++ src/layouts/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Welcome to Dragon News
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,15 @@
+import {
+  createBrowserRouter
+} from "react-router";
+import Root from "../layouts/Root/Root";
+import Home from "../layouts/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    children: [
+        {index:true,Component:Home}
+    ]
+  },
+]);
